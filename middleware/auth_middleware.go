bc0b090b2849a6395915b2311cdbf92b712bb44d
ilhambonardoo <ilhambonardo123@gmail.com>
2026-09-20 16:51:55 +0700

    feat: create middleware authentication based on apispec

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"golang-restful-api/helper"
+	"golang-restful-api/model/web"
+	"net/http"
+	"os"
+)
+
+type AuthMiddleware struct {
+	Handler http.Handler
+}
+
+func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
+	return &AuthMiddleware{Handler: handler}
+}
+
+func (middleware *AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	apiKey := os.Getenv("API_KEY")
+
+	if apiKey == "" {
+		apiKey = "RAHASIA"
+	}
+
+	if apiKey == request.Header.Get("X-API-KEY") {
+		middleware.Handler.ServeHTTP(writer, request)
+	} else {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusUnauthorized)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusUnauthorized,
+			Status: "UNAUTHORIZED",
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+	}
+}
